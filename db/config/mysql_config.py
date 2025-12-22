5516882c5b17079b6d05e1c44373937885fa6c72
poweryao <2384479796@qq.com>
2025-12-22 18:07:32 +0800

    删除了临时文件：migrations和db文件夹，修改了.env文件

--- db/config/mysql_config.py
+++
@@ -1,9 +0,0 @@
-# MySQL连接配置（共用）
-MYSQL_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "123456",  # 请替换为实际密码
-    "database": "artifact_db",
-    "charset": "utf8mb4",
-    "cursorclass": "pymysql.cursors.DictCursor"
-}
